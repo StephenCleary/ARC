7d1da612ab13f880e3309980d05a8bf42ae20327
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-04 21:01:39 -0400

    First draft of a lexer.

---
+++ src/Arc/Grammar/LiteralBoolToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arc.Values;
+
+namespace Arc.Grammar
+{
+    public sealed class LiteralToken : IToken
+    {
+        public LiteralToken(IValue value)
+        {
+            Value = value;
+        }
+
+        public IValue Value { get; private set; }
+    }
+}
---
+++ src/Arc/Grammar/MultiplicativeOperatorToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arc.Grammar
+{
+    public sealed class MultiplicativeOperatorToken : StringTokenBase
+    {
+        public MultiplicativeOperatorToken(string text)
+            : base(text)
+        {
+        }
+    }
+}
